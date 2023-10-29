08b13b9172a8ad268a062e23081a2cb6c7462431
SnailWhaleTester <91173736+SnailWhaleTester@users.noreply.github.com>
2023-10-29 17:57:32 -0600

    added sptify.js for later imports and profile

--- src/app/profile/[user]/page.js
+++ src/app/profile/[user]/page.js
@@ -1,19 +1,35 @@
-import React from 'react';
+"use client"
+import { getUserProfile } from '../../lib/spotify';
+import React, { useState, useEffect } from 'react';
 
-const Profile = () => {
+
+export default function Profile() {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    async function fetchUserProfile() {
+      const res = await getUserProfile();
+      const data = await res.json();
+      setProfile(data);
+    }
+
+    fetchUserProfile();
+  }, []);
+
   return (
-    <div className="profile">
-      <img
-        src="https://via.placeholder.com/150"
-        alt="User Profile"
-        className="profile-picture"
-      />
-      <div className="profile-info">
-        <h2>User Name</h2>
-        <p>Email: user@example.com</p>
-      </div>
+    <div>
+      {profile ? (
+        <div>
+          <h2>Profile Information</h2>
+          <p>Username: {profile.display_name}</p>
+          <p>Email: {profile.email}</p>
+          {profile.images.length > 0 && (
+            <img src={profile.images[0].url} alt={profile.display_name} />
+          )}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
-};
-
-export default Profile;
+}
